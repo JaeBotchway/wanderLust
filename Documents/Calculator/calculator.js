d0114de4dabf88ee8936dd4acdc251b9bc7e98a7
JaeBotchway <jacqueline.botchway@amalitech.org>
2021-08-12 10:54:04 +0000

    first commit

---
+++ Documents/Calculator/calculator.js
@@ -0,0 +1,16 @@
+
+class Calculator{
+    constructor(a,b){
+        this.a = a;
+        this.b = b;
+    }
+    
+    add(){
+        return this.a + this.b;
+    }
+    
+    product(){
+        return this.a * this.b;
+    }
+    }
+    module.exports = Calculator; 
